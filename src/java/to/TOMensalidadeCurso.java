c53b21ab05c9961e54cda4dda189fedc184cfede
Arthur Ribeiro <71500286@ALFAB16.BARROCA>
2018-11-19 19:52:56 -0200

    Commit inicial

---
+++ src/java/to/TOMensalidadeCurso.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package to;
+
+/**
+ *
+ * @author 71500286
+ */
+public class TOMensalidadeCurso {
+    private int id;
+    private TOCursos curso;
+    private double valor;
+    private double periodo;
+}
