fdf432c2d7502c90f96f0388e2b988a2f5bf5f99
Arthur Ribeiro <71500286@ALFAB16.BARROCA>
2018-11-19 19:50:07 -0200

    Commit inicial

---
+++ src/java/to/TOWorkshops.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package to;
+
+/**
+ *
+ * @author 71500286
+ */
+public class TOWorkshops {
+    private int id;
+    private String nome;
+    private String descricao;
+    private String data;
+}
